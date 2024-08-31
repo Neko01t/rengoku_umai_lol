5e97427f03bcac17a39f182a4a5ad7c1c36218a8
neko01t <omsonawane124421@gmail.com>
2024-08-31 22:39:44 +0530

    umaiiii update

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-// script.js
-document.querySelector('.donut').addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function () {
     let audio = document.getElementById('click-sound');
-    audio.play();
+    let image = document.querySelector('.donut');
+
+    audio.addEventListener('canplaythrough', function () {
+        document.querySelector('.container').style.visibility = 'visible';
+    });
+
+    image.addEventListener('load', function () {
+        document.body.style.background = 'linear-gradient(135deg, #ff3b3b, #ffd966, #f1c232, #cc0000, #990000)';
+    });
+    // Force image load (useful if image is cached)
+    if (image.complete) {
+        image.dispatchEvent(new Event('load'));
+    }
+    image.addEventListener('click', () => {
+        audio.play()
+    })
+    audio.load();
 });
